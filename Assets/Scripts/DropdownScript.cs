1c509ce2636585fa7e5fa2abc86a84ef8cb366af
Shweta Jha <84969672+SHWETA2JHA@users.noreply.github.com>
2023-02-17 22:13:39 +0530

    Update DropdownScript.cs

--- Assets/Scripts/DropdownScript.cs
+++ Assets/Scripts/DropdownScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class DropdownScript : MonoBehaviour
 {
     public void DropdownLoad(int index){
